cba35c01f2f334b98561911956cc170fe1fde0f8
Roberto99 <rab1002@alu.ubu.es>
2021-05-24 20:56:01 +0200

    Eliminación de atributos innecesarios de Node

--- src/lanSimulation/internals/Node.java
+++ src/lanSimulation/internals/Node.java
@@ -29,23 +29,6 @@ import java.io.Writer;
 public class Node {
 	// enumeration constants specifying all legal node types
 	/**
-	 * A node with type NODE has only basic functionality.
-	 */
-	public static final byte NODE = 0;
-	/**
-	 * A node with type WORKSTATION may initiate requests on the LAN.
-	 */
-	public static final byte WORKSTATION = 1;
-	/**
-	 * A node with type PRINTER may accept packages to be printed.
-	 */
-	public static final byte PRINTER = 2;
-
-	/**
-	 * Holds the type of the Node.
-	 */
-	private byte type_;
-	/**
 	 * Holds the name of the Node.
 	 */
 	private String name_;
